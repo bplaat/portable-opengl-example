da2d0ccd579b571626eae23dcd95edc4b8965d9d
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-06-23 19:28:04 +0200

    Fixed some small typo's

--- web/src/std.cpp
+++ web/src/std.cpp
@@ -3,8 +3,8 @@
 // A very simple malloc and free implementation for WebAssembly create by Bastiaan van der Plaat
 // Before every alloc block there is a uint32_t which contains the size of the following block
 // When allocated the top bit is set when freed the top bit is cleared so the block can be reused
-// When the end of memory is reach the memory is grow by pages until the next block can be allocated
-// This system is very slow O(n) I think because it loops over all blocks to find a new one but it is easy
+// When the end of memory is reach the memory is grow by x amount of pages until the next block can be allocated
+// This system is very slow O(n) I think because it loops over all blocks to find a new one but it is simple
 uint32_t *nextBlockHeader = (uint32_t *)&__heap_base;
 __attribute__((export_name("malloc"))) void *malloc(size_t size) {
     // Align all allocations to pointer size bytes
@@ -24,9 +24,10 @@ __attribute__((export_name("malloc"))) void *malloc(size_t size) {
 
     // When the next block header falls outside of the current memory grow it X amount of pages
     nextBlockHeader += sizeof(uint32_t) + alignedSize;
-    uint32_t memorySize = __builtin_wasm_memory_size(0) * WASM_PAGE_SIZE;
+    size_t memorySize = __builtin_wasm_memory_size(0) * WASM_PAGE_SIZE;
     if ((uintptr_t)nextBlockHeader > memorySize) {
-        __builtin_wasm_memory_grow(0, (((uintptr_t)nextBlockHeader - memorySize) + (WASM_PAGE_SIZE - 1)) / WASM_PAGE_SIZE);
+        size_t neededExtraSize = (uintptr_t)nextBlockHeader - memorySize;
+        __builtin_wasm_memory_grow(0, (neededExtraSize + (WASM_PAGE_SIZE - 1)) / WASM_PAGE_SIZE);
     }
 
     // When we can't find a freed block to reallocate allocate a new block at the end
